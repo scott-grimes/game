138a5c1b76a34a9636dc03345f16637b2188e3bd
Scott Grimes <scott-grimes@users.noreply.github.com>
2017-03-22 11:09:16 -0500

    Update login.py

--- login.py
+++ login.py
@@ -1,55 +1,108 @@
-import sqlite3
+import os, hashlib, sqlite3, binascii
 
-conn = sqlite3.connect(':memory:')
+db = sqlite3.connect('logins.db')
 
-print( "opened database sucessfully")
-
-conn.execute('''CREATE TABLE LOGINDATA
-(USERNAME BLOB PRIMARY KEY,
+db.execute('''CREATE TABLE IF NOT EXISTS LOGINDATA 
+    (USERNAME BLOB PRIMARY KEY,
 PASS BLOB NOT NULL,
 SALT BLOB NOT NULL);''')
 
-print( "table created successfully")
+def print_all_users():
+    cursor = db.execute("SELECT * from LOGINDATA")
+    for row in cursor:
+       print("USER = "+ row[0])
+       print ("PASS = "+ row[1])
+       print ("SALT = "+ row[2])
+       print("")
 
-conn.execute("INSERT INTO LOGINDATA (USERNAME,PASS,SALT) \
-	VALUES ('billy','password',10310)");
-conn.execute("INSERT INTO LOGINDATA (USERNAME,PASS,SALT) \
-	VALUES ('tom','password2',103112310)");
-conn.execute("INSERT INTO LOGINDATA (USERNAME,PASS,SALT) \
-	VALUES ('fuckoff','password3',55123)");
+def user_is_in_database(name):
+    cursor = db.execute("SELECT * from LOGINDATA WHERE USERNAME IS ?",(name,))
+    row = cursor.fetchone()
+    return (row is not None)
 
-conn.commit()
+def add_user_serverSide(input):
+    name = input[0]
+    password = input[1]
+    salt = input[2]
+    
+    if (not user_is_in_database(name)):
+        db.execute("INSERT INTO LOGINDATA (USERNAME,PASS,SALT) \
+    VALUES (?,?,?);",(name,password,salt))
+        db.commit()
+        return True
+    
+    return False
 
-print( "records created successfully")
+def checkPass(name, password):
+    cursor = db.execute("SELECT * from LOGINDATA WHERE USERNAME IS ?",(name,))
+    row = cursor.fetchone()
+    if row is not None:
+        hashed = hashlib.pbkdf2_hmac('sha256',password,binascii.unhexlify(row[2]),100000)
+        print "entered"+binascii.hexlify(hashed)
+        print "actual"+row[1]
+        if row[1] == binascii.hexlify(hashed):
+            return True
+    return False
 
-cursor = conn.execute("SELECT USERNAME, PASS, SALT from LOGINDATA")
-for row in cursor:
-   print ("USERNAME = ", row[0])
-   print ("PASS = ", row[1])
-   print ("SALT = ", row[2])
+def changePass_server_side(name, password, newPassword):
+    if(checkPass(name,password)):
+        
+        salt = os.urandom(16)
+        password = newPassword.encode()
+    
+        hashed_pass = hashlib.pbkdf2_hmac('sha256',password,salt,100000)
+        hashed_pass = binascii.hexlify(hashed_pass)
+        salt = binascii.hexlify(salt)
+        db.execute("""UPDATE LOGINDATA     
+            SET PASS = ?,    
+                SALT = ?
+            WHERE USERNAME = ?;""",(hashed_pass,salt,name,))
+        db.commit()
+        return True
+        
+    return False
 
-print ("Operation done successfully")
+def add_user_clientSide(name, password):
+    salt = os.urandom(16)
+    password = password.encode()
+    
+    hashed_pass = hashlib.pbkdf2_hmac('sha256',password,salt,100000)
+    hashed_pass = binascii.hexlify(hashed_pass)
+    salt = binascii.hexlify(salt)
+    
+    output = [name,hashed_pass,salt]
+    return add_user_serverSide(output)
 
-t_user = 'billy'
 
-cursor = conn.execute("SELECT * from LOGINDATA WHERE USERNAME IS '"+t_user+"'")
-for row in cursor:
-   print ("USERNAME = ", row[0])
-   print ("PASS = ", row[1])
-   print ("SALT = ", row[2])
-   
-   
-t_user = 'ralph'
 
-cursor = conn.execute("SELECT * from LOGINDATA WHERE USERNAME IS '"+t_user+"'")
-row = cursor.fetchone()
-if row is not None:
-   print ("USERNAME = ", row[0])
-   print ("PASS = ", row[1])
-   print ("SALT = ", row[2])
+
+choice = raw_input("Login : 1\n New User : 2\n Change Pass : 3 \n Print Users :4 ")
+if(choice == "1"):
+    print "Login Now:"
+    uName = raw_input("Username: ")
+    uPass = raw_input("Password: ")
+    if(checkPass(uName,uPass)):
+        print "Sucess"
+    else:
+        print "Incorrect Username or Password"
+
+elif(choice=="2"):
+    uName = raw_input("Username: ")
+    uPass = raw_input("Password: ")
+    if add_user_clientSide(uName,uPass):
+        print "Sucessfully added uName!"
+    else:
+        print "Username Taken!"
+elif(choice=="3"):
+    uName = raw_input("Username: ")
+    uPass = raw_input("Current Password: ")
+    uNewPass = raw_input("New Password: ")
+    if changePass_server_side(uName,uPass,uNewPass):
+        print "Sucessfully changed Password!"
+    else:
+        print "Your Username or Original Password is incorrect! Your password was not updated!"
+elif(choice=="4"):
+    print_all_users()
 else:
-  print("no user found")
+    print "invalid selection!"
 
-print ("Operation done successfully")
-
-conn.close()
